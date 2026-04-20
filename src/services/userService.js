7c2b0891fb89d73b9ca8477ef5acb46e8315fbba
Merve Tosun <myrve06@gmail.com>
2026-04-20 20:54:54 +0300

    PLAN-128: updatePassword metodu backend ile uyumlu olmasi icin PUT olarak guncellendi

--- src/services/userService.js
+++ src/services/userService.js
@@ -3,4 +3,4 @@ import api from './authService';
 export const getProfile = () => api.get('/users/me');
 export const getAllUsers = () => api.get('/users');
 export const updateProfile = (data) => api.put('/users/me', data);
-export const updatePassword = (data) => api.patch('/users/me/password', data);
+export const updatePassword = (data) => api.put('/users/me/password', data);
