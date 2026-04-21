bcb92e30c6147d24e1b87d98fb23ad536e7b8e0d
tahagunees <gnetaha55@gmail.com>
2026-04-22 00:36:35 +0300

    feat: implement registration page and add routing to App configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import {PrivacyPolicy, Tasks, Dashboard, Inbox, Login, ResetPassword, Register, Board, Projects, Team, Reports, Settings, CreateIssue, BackLog, Sprint} from "./pages";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/inbox" element={<Inbox />} />
         <Route path="/login" element={<Login />} />
